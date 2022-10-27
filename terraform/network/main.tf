05914a4aac420a6efca51a01672350650f0760e2
6691a <6691a@naver.com>
2022-10-27 14:49:45 +0000

    terraform aws vpc subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "vpc" {
   cidr_block       = local.vpc.cidr
-
+  enable_dns_hostnames  = true
   tags = {
     Name = local.vpc.name
   }
@@ -46,6 +46,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_eip" "ngw_ip" {
+  count = length(aws_subnet.public_subnet)
   vpc = true
 
   lifecycle {
@@ -54,16 +55,17 @@ resource "aws_eip" "ngw_ip" {
 }
 
 resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw_ip.id
-  # public subnet의 first(192.168.0.0/24) id 
-  subnet_id = aws_subnet.public_subnet[local.subnet_group.public.subnets[0].cidr].id
+  count = length(aws_eip.ngw_ip)
+
+  allocation_id = aws_eip.ngw_ip[count.index].id
+  subnet_id = aws_subnet.public_subnet[local.subnet_group.public.subnets[count.index].cidr].id
 
   tags = {
     Name = "MySQL_NAT Gateway"
   }
 }
 
-
+# private route table
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
 
@@ -75,17 +77,19 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route_table_association" "private_route_tables" {
   for_each = aws_subnet.private_subnet
   
-  subnet_id      =  each.value.id
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.private_route_table.id
 }
-resource "aws_route" "private_rt_route" {
-    route_table_id              = aws_route_table.private_route_table.id
-    destination_cidr_block      = "0.0.0.0/0"
-    nat_gateway_id              = aws_nat_gateway.ngw.id
+
+resource "aws_route" "private_route" {
+  route_table_id              = aws_route_table.private_route_table.id
+  destination_cidr_block      = "0.0.0.0/0"
+  nat_gateway_id              = aws_nat_gateway.ngw[0].id
 }
 
-
+# public route table
 resource "aws_default_route_table" "public_route_table" {
+
   default_route_table_id = aws_vpc.vpc.default_route_table_id
 
     route {
@@ -99,45 +103,45 @@ resource "aws_default_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_route_tables" {
-  for_each = aws_subnet.private_subnet
+  for_each = aws_subnet.public_subnet
   
   subnet_id      =  each.value.id
   route_table_id = aws_default_route_table.public_route_table.id
 }
 
-resource "aws_security_group" "ssh" {
-  name        = "${aws_vpc.vpc.tags.Name}-ssh"
-  description = "Security Group for SSH"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    description = "Allow SSH from anywhere."
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-}
-
-# resource "aws_security_group" "mysql" {
-#   name        = "${aws_vpc.vpc.tags.Name}-mysql"
-#   description = "Security Group for MySQL"
+# resource "aws_security_group" "ssh" {
+#   name        = "${aws_vpc.vpc.tags.Name}-ssh"
+#   description = "Security Group for SSH"
 #   vpc_id      = aws_vpc.vpc.id
 
 #   ingress {
-#     description ="Allow MySQL from anywhere."   
-#     from_port        = 3306
-#     to_port          = 3306
+#     description = "Allow SSH from anywhere."
+#     from_port = 22
+#     to_port = 22
 #     protocol = "tcp"
 #     cidr_blocks = ["0.0.0.0/0"]
 #   }
-
-#    egress {
-#     description = "Allow to communicate to the Internet."
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = [ "0.0.0.0/0" ]
-#    }
+  
 # }
+
+# # resource "aws_security_group" "mysql" {
+# #   name        = "${aws_vpc.vpc.tags.Name}-mysql"
+# #   description = "Security Group for MySQL"
+# #   vpc_id      = aws_vpc.vpc.id
+
+# #   ingress {
+# #     description ="Allow MySQL from anywhere."   
+# #     from_port        = 3306
+# #     to_port          = 3306
+# #     protocol = "tcp"
+# #     cidr_blocks = ["0.0.0.0/0"]
+# #   }
+
+# #    egress {
+# #     description = "Allow to communicate to the Internet."
+# #     from_port = 0
+# #     to_port = 0
+# #     protocol = "-1"
+# #     cidr_blocks = [ "0.0.0.0/0" ]
+# #    }
+# # }
